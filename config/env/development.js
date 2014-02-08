fdb19418261bef67fd7b1b156667710c94119618
anjosh <joshuaan1@gmail.com>
2014-02-08 03:27:29 -0500

    fixing heroku

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://joshuaan1@gmail.com:auhsoj@troup.mongohq.com:10018/app22024478',
+    db: 'mongodb://joshuaan1@gmail.com:auhsoj@troup.mongohq.com:10022/app22025592',
     app: {
         name: 'MEAN - A Modern Stack - Development'
     },
